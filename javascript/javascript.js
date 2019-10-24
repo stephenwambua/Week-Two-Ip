be6344f711058b6b045416960f9ef98ae54ef34c
stephen wambua <ngumbausteph@gmail.com>
2019-10-24 12:02:41 +0300

    add javascript directory and javascript.js file

---
+++ javascript/javascript.js
@@ -0,0 +1,16 @@
+function printMaleName(nameofperson){
+    if(Monday){
+        console.log("Your name is Kwadwo you were born on a monday");
+    }else if(Tuesday){
+        console.log("Your name is Kwabena you were born on a monday");
+    }else if( Wednesday){
+        console.log("Your name is Kwaku you were born on a monday");
+    }else if(Thursday){
+        console.log("Your name is Yaw you were born on a monday");
+    }else if(Friday){
+        console.log("Your name is Kofi you were born on a monday");
+    }else if( Saturday){
+      console.log("Your name is Kwame you were born on a monday");
+    }else(Sunday){
+        console.log("Your name is Kwasi you were born on a monday");
+    }
