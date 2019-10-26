31fdc4993bae635586e1c931a8f493865c8492ab
stephen wambua <ngumbausteph@gmail.com>
2019-10-26 19:03:37 +0300

    add if ele statement and variable weekdays

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -7,28 +7,27 @@ submit.addEventListener("click", function(e){
     var yob = document.getElementById("year").value;
     var gent = document.getElementById("male").value;
     var lady = document.getElementById("female").value;
+    var weekdays = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+    var man = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var woman =["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
+//     function validationForm(){
+//         var x = document.forms["myForm"]["fname"].value;
+//   if (x == "") {
+//     alert("Name must be filled out");
+//     return false;
+//     }
     // var formula =( ( (cent+1/4) - 2 * (cent - 1)) + ((5*yobs/4) ) + ((26*(mob+1)/10)) + dob)% 7;
 
+    // this is the area that will receive values inputed
     var date = new Date(`${yob}-${mob}-${dob}`); //yob + "-" + mob + "-" + dob
+    // here it provides us with the day in form of integers
     var day = date.getDay();
-        
-    alert(day);
-    
+    if (gent.checked) {  
+        alert("your name is " + man[day]); 
+    }else{ 
+        alert("your name is " + woman[day]);
+    }
 
+    // alert(day);
 })
-
-// document.write("formula");
-
-// function myForm(){
-
-//     var day =parseInt(document.getElementById("date").Value;);
-
-//     return document.getElementById("result").innerHTML= day;
-
-// }
-// CC - is the century digits. For example 1989 has CC = 19
-// YY - is the Year digits (1989 has YY = 89)
-// MM -  is the Month
-// DD - is the Day of the month 
-// mod - is the modulus function ( % )
