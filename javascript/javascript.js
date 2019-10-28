5540f84ac048d2acbb9d6b6b788bc62ef9390ea0
stephen wambua <ngumbausteph@gmail.com>
2019-10-28 12:52:11 +0300

    add the document.getelementbyid and removed the alerts

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,34 +1,46 @@
 var submit = document.getElementById("submit");
-
 submit.addEventListener("click", function(e){
     e.preventDefault();
     var dob = document.getElementById("date").value;
     var mob = document.getElementById ("month").value;
     var yob = document.getElementById("year").value;
-    var gent = document.getElementById("male").value;
-    var lady = document.getElementById("female").value;
+    var sex = document.getElementsByName("gender").valu;
+    var gent = document.getElementById("male");
+    var lady = document.getElementById("female");
     var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     var man = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     var woman =["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
-    if (dob == "") {
-    alert("Date must be filled out");
+    if (dob <1 || dob>31){
+    alert("Enter a valid date");
     return false;
-    }else if (mob == "") {
-    alert("Date must be filled out")
+    }else if (mob <1 || mob>12) {
+    alert("Enter a valid month")
+    return false;
+    }else if(yob <1 || dob>2019){
+    alert("Enter a valid year")
+    return false;
+    }else if (mob ==2 && dob>29){
+    alert("Enter a valid date")
+    return false;
     }
-
+    if (sex == "") {
+     alert("Enter a valid gend");   
+     return false;
+    }
+    console.log(sex);
     // var formula =( ( (cent/4) - 2 * (cent - 1)) + ((5*yobs/4) ) + ((26*(mob+1)/10)) + dob)% 7;
     // this is the area that will receive values inputed
     var date = new Date(`${yob}-${mob}-${dob}`); //yob + "-" + mob + "-" + dob
     // here it provides us with the day in form of integers
     var day = date.getDay();
-    if (gent.checked) {  
-        alert("your Akan name is " + man[day] + " which means you were born on "+ weekdays[day]); 
+    if(gent.checked) {  
+        // alert("your Akan name is " + man[day] + " which means you were born on "+ weekdays[day]); 
+        document.getElementById("results").innerHTML = man[day];
     }
     else{ 
-        alert("your Akan name is " + woman[day] + " which means you were born on " + weekdays[day]);
+        // alert("your Akan name is " + woman[day] + " which means you were born on " + weekdays[day]);
+        document.getElementById("results").innerHTML = woman[day];
     }
-
     // alert(day);
 })
