3131895cfc2cf31b013f7630449ff6f29461e0ff
stephen wambua <ngumbausteph@gmail.com>
2019-10-28 09:20:45 +0300

    add new script to the js file to give an alert if the fileds are not filled

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -7,26 +7,27 @@ submit.addEventListener("click", function(e){
     var yob = document.getElementById("year").value;
     var gent = document.getElementById("male").value;
     var lady = document.getElementById("female").value;
-    var weekdays = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+    var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     var man = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     var woman =["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
-//     function validationForm(){
-//         var x = document.forms["myForm"]["fname"].value;
-//   if (x == "") {
-//     alert("Name must be filled out");
-//     return false;
-//     }
-    // var formula =( ( (cent+1/4) - 2 * (cent - 1)) + ((5*yobs/4) ) + ((26*(mob+1)/10)) + dob)% 7;
+    if (dob == "") {
+    alert("Date must be filled out");
+    return false;
+    }else if (mob == "") {
+    alert("Date must be filled out")
+    }
 
+    // var formula =( ( (cent/4) - 2 * (cent - 1)) + ((5*yobs/4) ) + ((26*(mob+1)/10)) + dob)% 7;
     // this is the area that will receive values inputed
     var date = new Date(`${yob}-${mob}-${dob}`); //yob + "-" + mob + "-" + dob
     // here it provides us with the day in form of integers
     var day = date.getDay();
     if (gent.checked) {  
-        alert("your name is " + man[day]); 
-    }else{ 
-        alert("your name is " + woman[day]);
+        alert("your Akan name is " + man[day] + " which means you were born on "+ weekdays[day]); 
+    }
+    else{ 
+        alert("your Akan name is " + woman[day] + " which means you were born on " + weekdays[day]);
     }
 
     // alert(day);
